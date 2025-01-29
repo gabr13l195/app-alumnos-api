35329085c5701f6d5e34b6d72a6472f262d3958d
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-01-29 16:45:46 -0500

    First commit

---
+++ src/main/java/com/springapp/app_cliente/Controlador/AlumnoControlador.java
@@ -0,0 +1,26 @@
+package com.springapp.app_cliente.Controlador;
+
+import com.springapp.app_cliente.Entidad.Alumno;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
+public class AlumnoControlador {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    //NO ENTENDER
+    @GetMapping("/alumnos")
+    public String listarAlumnos(Model model){
+        List<Alumno> alumnos = restTemplate.getForObject("http://localhost:8080/api/alumnos", List.class);
+        model.addAttribute("Alumno", alumnos);
+        return "alumnos";
+    }
+}
---
+++ src/main/java/com/springapp/app_cliente/Entidad/Alumno.java
@@ -0,0 +1,12 @@
+package com.springapp.app_cliente.Entidad;
+
+
+import lombok.Data;
+
+@Data
+public class Alumno {
+    private int id;
+    private String nombre;
+    private String ciudad;
+    private int edad;
+}
